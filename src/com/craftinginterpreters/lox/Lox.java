9601ed0eb20c7eb4197f59ac31057889d5ca8817
astrilo-monk <mdowaisfarhanakhter561@gmail.com>
2026-08-10 12:18:53 +0530

    update: added node_modules to .gitignore (adder prettier extension for bakchodi)

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
-import java.nio.file.Files;
-
+import java.nio.file.Files
 import java.util.List;
 import java.util.Scanner;
 
